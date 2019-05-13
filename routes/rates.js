712f83edfa5882b4620fc69f8843c47cc7c10b38
nikolal <iamnikolalukic@gmail.com>
2019-05-13 16:59:14 +0200

    Remove test push

--- routes/rates.js
+++ routes/rates.js
@@ -5,6 +5,7 @@ import { sendPush } from '../services/pushNotifications.js';
 // console.log('test')
 
 router.post('/', function(req, res, next) {
+  console.log(req.body.pushToken, req.body.type);
   sendPush([req.body.pushToken], req.body.type);
   res.json({'rates': 'router'});
 });
